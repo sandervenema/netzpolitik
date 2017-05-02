9c2750929c637468b84d2aa13d7f93cf0c63e279
Sander Venema <sander@sandervenema.ch>
2017-05-02 20:30:02 +0200

    Styled some more and fixed confirm mail

--- petitions/urls.py
+++ petitions/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^sign/$', views.sign, name='sign'),
-    url(r'^confirm/([0-9a-z]{64})/$', views.confirm, name='confirm')
+    url(r'^confirm/([0-9a-z]{64})/$', views.confirm, name='confirm'),
 ]
