49b300c878c7186750b109c434d92aac2d790e54
Sander Venema <sander@sandervenema.ch>
2017-05-02 18:43:45 +0200

    Changed url to /openletter

--- netzpolitik/urls.py
+++ netzpolitik/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'statement/', include('petitions.urls')),
+    url(r'openletter/', include('petitions.urls')),
 ]
