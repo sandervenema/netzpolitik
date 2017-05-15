334a26f69b380a2905c1d1ffa853287b3aec962a
Sander Venema <sander@sandervenema.ch>
2017-05-15 20:59:45 +0200

    Removed debug code

--- netzpolitik/urls.py
+++ netzpolitik/urls.py
@@ -1,14 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.conf import settings
 
 urlpatterns = [
     url(r'openletter/', include('petitions.urls')),
     url(r'openletter/i18n/', include('django.conf.urls.i18n')),
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
