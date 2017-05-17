814353a0f79d295e5f5644915c37d8ab12c40f45
Sander Venema <sander@sandervenema.ch>
2017-05-17 15:45:15 +0200

    Added admin app to settings.py

--- netzpolitik/settings.py
+++ netzpolitik/settings.py
@@ -57,7 +57,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'petitions'
+    'django.contrib.admin',
+    'petitions',
 ]
 
 MIDDLEWARE_CLASSES = [
